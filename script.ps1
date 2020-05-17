f99fbfb48291ad50a69a1045888ec409e05175c1
Dexter Dixon <dexterdixon561@gmail.com>
2020-05-17 15:06:01 -0400

    Added comments

--- script.ps1
+++ script.ps1
@@ -1,13 +1,16 @@
+#Initializing Variables
 $Desktop = "C:\Users\user\Desktop"
 $Downloads = "C:\Users\user\Downloads"
 $Directories = $Downloads, $Desktop
 
+#Creating Hash Tables for Destination Directories
 $destinations = @{
     Pictures = "C:\Users\user\Pictures\downloaded"
     Documents = "C:\Users\user\Documents\downloaded"
     Videos =  "C:\Users\user\Videos\downloaded"
     Music = "C:\Users\user\Music\downloaded"
 }
+#Creating Hash Tables for File Extensions
 $extensions = @{
     Pictures = '.png', '.jpeg', '.jpg', '.gif'
     Documents = '.doc', '.docx', '.pdf', '.mht', '.html', '.xls', '.xlsx', '.csv'
@@ -15,11 +18,14 @@ $extensions = @{
     Music = '.mp3', '.wma', '.wav'
 }
 
+#Loops through each directory in Directories variable
 foreach ($directory in $Directories)
 {
+    #Gets a list of all the files in the directory and sub directories
     $Files = Get-ChildItem $directory -File -Recurse
     foreach($file in $Files)
     {
+        #Checks each files extension and sends it to the appropriate destination
         if ($extensions.Music -contains $file.Extension)
         {
             Move-Item -Path $file.FullName -Destination $destinations.Music -Force
